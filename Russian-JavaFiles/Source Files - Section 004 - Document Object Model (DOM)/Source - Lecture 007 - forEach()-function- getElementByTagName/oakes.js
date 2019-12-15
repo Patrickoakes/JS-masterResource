bbde71f5c6796ddff461705149de81af1d3a18c7
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-15 06:55:20 -0800

    commit

---
+++ Russian-JavaFiles/Source Files - Section 004 - Document Object Model (DOM)/Source - Lecture 007 - forEach()-function- getElementByTagName/oakes.js
@@ -0,0 +1,9 @@
+/*var icons = document.getElementsByClassName("fa");
+
+Array.from(icons).forEach(function(icon) {
+  console.log(icon);
+}); */
+
+var lis = document.getElementByTagName("li");
+
+console.log(lis);
