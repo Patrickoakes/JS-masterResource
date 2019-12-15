8a1c85aad90a1b38cec7fab12cb814698034ecdc
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-14 19:28:04 -0800

    Commit Multi Element

---
+++ Russian-JavaFiles/Source Files - Section 004 - Document Object Model (DOM)/Source - Lecture 006 - GetAnd Manipulate On Multiple Elements - Part 1/oakes.js
@@ -0,0 +1,14 @@
+/*var icons = document.getElementsByClassName("fa");
+
+for (var i = 0; i < icons.length; i++) {
+  console.log(icons[i]);
+}
+
+//icons.push("Hello");   ERROR
+
+var iconsArr = Array.from(icons);
+console.log(iconsArr);
+console.log(icons);
+
+iconsArr.push("Hello");
+console.log(iconsArr); */
