e99a36df197728786f43e8c7b8c688ddaf004c84
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-15 13:58:40 -0800

    Event

---
+++ Russian-JavaFiles/Source Files - Section 004 - Document Object Model (DOM)/Source - Lecture 011 - onClick-mouseover/oakes.js
@@ -0,0 +1,27 @@
+var h2 = document.querySelector("header h2");
+
+/*h2.onclick = function() {
+    console.log("Clicked");
+  };
+  
+  h2.onmouseover = function() {
+    console.log("Mouseover");
+  }; */
+
+//  SECOND WAY WITH HTML
+/* function a() {
+  console.log("Clicked");
+};
+
+function b() {
+  console.log("Mouseover");
+}; */
+
+///  Only a is executed /////
+function a() {
+  console.log("Clicked a");
+}
+
+function b() {
+  console.log("Clicked b");
+}
