11cc9c56648675600fa6ba46ef5581d6928b7711
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-15 21:29:23 -0800

    event 3

--- Russian-JavaFiles/Source Files - Section 004 - Document Object Model (DOM)/Source - Lecture 013 - Events - Part 3/oakes.js
+++ Russian-JavaFiles/Source Files - Section 004 - Document Object Model (DOM)/Source - Lecture 013 - Events - Part 3/oakes.js
@@ -1,7 +1,45 @@
+/*var h2 = document.querySelector("header h2");
+
+h2.addEventListener("click", function() {
+  a(5, 10);
+});
+
+function a(x, y) {
+  console.log(x * y);
+} */
+
+var div = document.querySelector("div.wrapper");
+var header = document.querySelector("header");
 var h2 = document.querySelector("header h2");
 
-h2.addEventListener("click", a);
+div.addEventListener(
+  "click",
+  function() {
+    var delay = new Date().getTime() + 1000;
+    while (new Date() < delay) {}
+    console.log("From div");
+  },
+  true
+);
 
-function a() {
-  console.log(this);
-}
+header.addEventListener(
+  "click",
+  function() {
+    var delay = new Date().getTime() + 1000;
+    while (new Date() < delay) {}
+    console.log("From header");
+  },
+  true
+);
+
+h2.addEventListener(
+  "click",
+  function() {
+    var delay = new Date().getTime() + 1000;
+    while (new Date() < delay) {}
+    console.log("From h2");
+  },
+  true
+);
+// inner to outer element = event bubbling\
+// outer to inner=  event capture
